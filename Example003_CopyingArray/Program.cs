9ff70afc7d9884cd44e475ccc927e398ce5f9057
Денис <denis070619870@rambler.ru>
2022-08-22 16:58:05 +0300

    .\Example003_CopyingArray

---
+++ Example003_CopyingArray/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу копирования массива
+
+public class Example
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5, 7, 77 };
+        int i = 0;
+        int length = arr.Length;
+        while (i < length)
+        {
+            Console.WriteLine(arr[i]);
+            i++;
+        }
+        int j = 0;
+        int[] copy = new int[length];
+        Array.Copy(arr, j, copy, 0, length);
+        Console.WriteLine("Копия массива: ");
+        Console.WriteLine(copy);
+    }
+}
