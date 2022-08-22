ded804add9f9ba26a1fe19228ef5db1c8f6e6230
Денис <denis070619870@rambler.ru>
2022-08-22 17:43:23 +0300

    Example002_ScalingTheShape

--- Example002_ScalingTheShape/Program.cs
+++ Example002_ScalingTheShape/Program.cs
@@ -1,86 +1,36 @@
-﻿// Написать программу масштабирования фигуры
+﻿
+int xa = 1, ya = 1,
+    xb = 41, yb = 8,
+    xc = 60, yc = 10;
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
- 
-namespace CompGR13
+Console.SetCursorPosition(xa, ya);
+Console.Write("+");
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
+
+int x = xa, y =ya;
+int count =0;
+while(count < 10000)
 {
-    public partial class Form1 : Form
+    int what = new Random().Next(0, 3);
+    if(what == 0)
     {
-        private Point[] Points = new Point[4];
-        public Form1()
-        {
-            InitializeComponent();
-        }
- 
-       private void Form1_Load(object sender, EventArgs e)
-        {
-            Points[0] = new Point(50,100);
-            Points[1] = new Point(100,50);
-            Points[2] = new Point(150,100);
-            Points[3] = new Point(100,150);
-            
- 
-            Draw();
- 
-        }
- 
-        private void Draw()
-        {
-            Bitmap bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
-            Graphics g = Graphics.FromImage(bmp);
- 
-            g.DrawPolygon(Pens.Black, Points);
-            
- 
-            pictureBox1.Image = bmp;
-        }
- 
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode)
-            {
-                case Keys.Left:
-                    MoveFigure(x: -5);
-                    break;
-                case Keys.Right:
-                    MoveFigure(x: 5);
-                    break;
-                /*case Keys.Up:
-                    MoveFigure(y: -5);
-                    break;
-                case Keys.Down:
-                    MoveFigure(y: 5);
-                    break;*/
-                default:
-                    break;
-            }
- 
-            void MoveFigure(int x = 0, int y = 0)
-            {
-                for (int i = 0; i < Points.Length; i++)
-                {
-                    Points[i].X += x;
-                    Points[i].Y += y;
-                }
- 
-                Draw();
-            }
- 
-            void ResizeFigure(int x = 0, int y = 0)
-            {
-                
-            }
- 
- 
-        }
- 
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
     }
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
 }
