20407518e72af2d09b7f89d3c6193bd2c7869e62
Денис <denis070619870@rambler.ru>
2022-08-30 18:36:23 +0300

    Example001_IntersectionPoint

--- Example001_IntersectionPoint/Program.cs
+++ Example001_IntersectionPoint/Program.cs
@@ -11,12 +11,25 @@
 
 Console.WriteLine(" ");
 
-int k1 = 5;  
-int k2 = 3;  
-int b1 = 4;
-int b2 = 2;
+//double k1 = 5;  
+//double k2 = 3;  
+//double b1 = 4;
+//double b2 = 2;
+
+Console.WriteLine("Введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
  
-int x = (b2 - b1)/(k1 - k2);
+double x = (b2 - b1)/(k1 - k2);
+//double y = k2 * x + b2;
+double y = k1 * x + b1;
  
 Console.WriteLine();
-Console.WriteLine($"Точка пересечения: {x}; {k1*x + b1} ");
+Console.WriteLine($"Точка пересечения: X = {x}; Y = {y} ");
